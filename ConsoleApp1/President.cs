fdc183aa2d15ff49896fcce26163b1f2f85a2d66
Filip-Ericsson <filip.ericsson@elev.ga.ntig.se>
2019-09-26 14:32:38 +0200

    Second Push

---
+++ ConsoleApp1/President.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace President
+{
+
+    class President
+    {
+        public string name = "Arcturus Mengsk";
+        private int environmentPriority;
+        private int defensePriority;
+        private int healthcarePriority;
+        private int corruption;
+        private int money;
+
+        Random gen = new Random();
+
+        public President(string n)
+        {
+            environmentPriority = gen.Next(0, 100);
+            defensePriority = gen.Next(0, 100);
+            healthcarePriority = gen.Next(0, 100);
+        }
+
+        public void Present()
+        {
+            Console.WriteLine("President: " + name);
+            Console.WriteLine("The current Environment priority is: " + environmentPriority);
+            Console.WriteLine("The current Defense priority is: " + defensePriority);
+            Console.WriteLine("The current Health priority is: " + healthcarePriority);
+        }
+
+        public bool PassLegislation(int e, int d, int h, int bribe)
+        {
+            environmentPriority = environmentPriority + bribe;
+            defensePriority = defensePriority + bribe;
+            healthcarePriority = healthcarePriority + bribe;
+            if(environmentPriority > e && defensePriority > d && healthcarePriority > h)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,48 @@ namespace President
     {
         static void Main(string[] args)
         {
+            President president = new President("Arcturus Mengsk");
+
+            while (true)
+            {
+                Console.WriteLine("Hello and welcome to President");
+                Console.WriteLine("Press 1 to Present the Presidents values");
+                Console.WriteLine("Press 2 to Pass Leglislation");
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    president.Present();
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("How much to put in enviroment, 0-100");
+
+                    int.TryParse(Console.ReadLine(),out int e);
+                    Console.WriteLine("How much to put in defence, 0-100");
+                    int.TryParse(Console.ReadLine(), out int d);
+                    Console.WriteLine("How much to put in healthcare, 0-100");
+                    int.TryParse(Console.ReadLine(), out int h);
+                    Console.WriteLine("Please, give some money, 0-100");
+                    int.TryParse(Console.ReadLine(), out int bribe);
+
+
+
+                    if (president.PassLegislation(e, d, h, bribe) == true)
+                    {
+                        Console.WriteLine("The law passed!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The President rejects your leglislation");
+                    }
+
+
+
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
